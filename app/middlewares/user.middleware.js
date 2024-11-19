23876163865ea99997ac1ebdf6269d1d3bfbd6c7
abhijitsy-90 <abhijitsy20001@gmail.com>
2024-11-19 09:25:43 +0530

    all code

--- app/middlewares/user.middleware.js
+++ app/middlewares/user.middleware.js
@@ -1,24 +1,4 @@
 const jwt = require('jsonwebtoken');
-
-// const authenticate = (req, res, next) => {
-//   const token = req.header('Authorization')?.split(' ')[1];
-
-//   if (!token) {
-//     return res.status(403).json({ message: 'No token provided' });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.SECRETKEY);
-//     req.user = decoded;
-//     next();
-//   } catch (err) {
-//     res.status(401).json({ message: 'Invalid token' });
-//   }
-// };
-
-// module.exports = { authenticate };
-
-
 const secret = process.env.SECRETKEY || "default";                                                                                                                                                                                                                 
 const authenticate = (req, res, next) => {
   const token = req.headers.authorization;
